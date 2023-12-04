01bdafde7c0c724bb13870ee0bfce8d27cc3078e
enrico <enrico.dsf@gmail.com>
2023-12-04 23:16:30 +0100

    more concise day4 solution

--- day4/solution.py
+++ day4/solution.py
@@ -1,17 +1,14 @@
-import re
-
 with open("data") as f:
-    lines = f.read().splitlines()
+    lines = [l.split() for l in f.readlines()]
 
 
 # ========== PART 1 & 2 ==========
 points = 0
 cards_num = [1] * len(lines)
-for i, line in enumerate(lines):
-    winning_nums, my_nums = map(str.split, re.sub(r"Card\s+\d:", "", line).split("|"))
-    my_winning_nums = len(set(winning_nums) & set(my_nums))
+for i, numbers in enumerate(lines):
+    my_winning_nums = len(set(numbers[2:12]) & set(numbers[13:]))
     points += 2**my_winning_nums // 2
-    for j in range(i + 1, i + my_winning_nums + 1):
-        cards_num[j] += cards_num[i]
+    for j in range(my_winning_nums):
+        cards_num[i + j + 1] += cards_num[i]
 
 print(points, sum(cards_num), sep="\n")
