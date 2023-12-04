2ac0b100b9773c3f5bcf48023f0f4590baba759c
enrico <enrico.dsf@gmail.com>
2023-12-04 22:09:51 +0100

    day4 solution minor refactor

--- day4/solution.py
+++ day4/solution.py
@@ -8,12 +8,10 @@ with open("data") as f:
 points = 0
 cards_num = [1] * len(lines)
 for i, line in enumerate(lines):
-    winning_numbers, my_numbers = map(
-        str.split, re.sub(r"Card\s+\d:", "", line).split("|")
-    )
-    my_winning_numbers = len(set(winning_numbers) & set(my_numbers))
-    points += 2**my_winning_numbers // 2
-    for j in range(i + 1, i + my_winning_numbers + 1):
+    winning_nums, my_nums = map(str.split, re.sub(r"Card\s+\d:", "", line).split("|"))
+    my_winning_nums = len(set(winning_nums) & set(my_nums))
+    points += 2**my_winning_nums // 2
+    for j in range(i + 1, i + my_winning_nums + 1):
         cards_num[j] += cards_num[i]
 
 print(points, sum(cards_num), sep="\n")
