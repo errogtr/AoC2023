d14eac211fb81f6675bcef145a4cd2cbb457dcbf
enrico <enrico.dsf@gmail.com>
2023-12-06 15:14:26 +0100

    day4 minor refactor

--- day4/solution.py
+++ day4/solution.py
@@ -1,12 +1,15 @@
-with open("data") as f:
-    lines = [l.split() for l in f.readlines()]
+import re
+
+with open("example") as f:
+    lines = f.readlines()
 
 
 # ========== PART 1 & 2 ==========
 points = 0
 cards_num = [1] * len(lines)
 for i, numbers in enumerate(lines):
-    my_winning_nums = len(set(numbers[2:12]) & set(numbers[13:]))
+    winning_nums, my_nums = map(str.split, numbers.split("|"))
+    my_winning_nums = len(set(winning_nums[2:]) & set(my_nums))
     points += 2**my_winning_nums // 2
     for j in range(my_winning_nums):
         cards_num[i + j + 1] += cards_num[i]
