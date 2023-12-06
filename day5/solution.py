9cf5e9a16f566517511ecbd9c874c3a570a87fba
enrico <enrico.dsf@gmail.com>
2023-12-06 19:50:43 +0100

    day5 refactor

--- day5/solution.py
+++ day5/solution.py
@@ -1,31 +1,30 @@
 import re
 
 
-def locations(seed_intervals):
+def locations(intervals):
     for maps_block in input_maps.split("\n\n"):
         mappings = maps_block.split("\n")[1:]
         images = list()
-        while seed_intervals:
-            x, y = seed_intervals.pop()
+        while intervals:
+            x, y = intervals.pop()
             for mapping in mappings:
                 a, b, delta = map(int, mapping.split())
-                if b <= x <= y < b + delta:
+                r_endpoint = b + delta - 1
+                if b <= x <= y <= r_endpoint:
                     images.append((x - b + a, y - b + a))
                     break
-                if b <= x < b + delta < y:
-                    seed_intervals.extend([(x, b + delta - 1), (b + delta, y)])
+                if b <= x <= r_endpoint < y:
+                    intervals.extend([(x, r_endpoint), (r_endpoint + 1, y)])
                     break
             else:
                 images.append((x, y))
-        seed_intervals = images
-    return seed_intervals
+        intervals = images
+    return intervals
 
 
 with open("data") as f:
     input_seeds, input_maps = f.read().split("\n\n", 1)
 
-from time import time
-start = time()
 # ========= PART 1 ==========
 seed_data = [int(x) for x in re.findall(r"\d+", input_seeds)]
 print(min(min(locations([(x, x) for x in seed_data]))))
@@ -33,4 +32,3 @@ print(min(min(locations([(x, x) for x in seed_data]))))
 # ========= PART 2 ==========
 seed_intervals = [(x, x + d - 1) for x, d in zip(seed_data[::2], seed_data[1::2])]
 print(min(min(locations(seed_intervals))))
-print("Elapsed:", round(1000*(time() - start)), "ms")
