5b557a9c701d4dca1d363f20aaccff2ce5d37bdc
enrico <enrico.dsf@gmail.com>
2023-12-09 13:38:31 +0100

    day9 solution

---
+++ day9/solution.py
@@ -0,0 +1,20 @@
+from operator import sub
+from itertools import pairwise
+
+
+def get_next(sequence):
+    last = sequence[-1]
+    while any(sequence):
+        sequence = [sub(y, x) for x, y in pairwise(sequence)]
+        last += sequence[-1]
+    return last
+
+
+with open("data") as f:
+    sequences = [[int(x) for x in l.split()] for l in f.readlines()]
+
+# ========= PART 1 =========
+print(sum(get_next(s) for s in sequences))
+
+# ========= PART 2 =========
+print(sum(get_next(s[::-1]) for s in sequences))
