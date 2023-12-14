0f0af8490b2a0bc1d830e608bd98eb13cfc58d94
enrico <enrico.dsf@gmail.com>
2023-12-14 01:46:59 +0100

    day13 optimized

--- day13/solution.py
+++ day13/solution.py
@@ -1,13 +1,11 @@
-def symmetry(p):
-    N = len(p)
+def symmetry(p, N):
     for i in range(1, N):
         if all(p[i - j - 1] == p[i + j] for j in range(min(i, N - i))):
             return i
     return 0
 
 
-def smudge_symmetry(p):
-    N = len(p)
+def smudge_symmetry(p, N):
     for i in range(1, N):
         if sum(hamming(p[i - j - 1], p[i + j]) for j in range(min(i, N - i))) == 1:
             return i
@@ -18,10 +16,12 @@ def hamming(X, Y):
     return sum(x != y for x, y in zip(X, Y))
 
 
-def summarize(pattern, func):
+def summarize(pattern, smudge=False):
     rows = pattern.split()
     cols = ["".join(line) for line in zip(*rows)]
-    return 100 * func(rows) + func(cols)
+    if smudge:
+        return 100 * smudge_symmetry(rows, len(rows)) or smudge_symmetry(cols, len(cols))
+    return 100 * symmetry(rows, len(rows)) or symmetry(cols, len(cols))
 
 
 with open("data") as f:
@@ -29,8 +29,8 @@ with open("data") as f:
 
 
 # ========= PART 1 =========
-print(sum(summarize(pattern, symmetry) for pattern in patterns))
+print(sum(summarize(pattern) for pattern in patterns))
 
 
 # ========= PART 2 =========
-print(sum(summarize(pattern, smudge_symmetry) for pattern in patterns))
+print(sum(summarize(pattern, smudge=True) for pattern in patterns))
