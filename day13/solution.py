6013bd1a405207a325fa6f16ed24ce27c63b85ec
enrico <enrico.dsf@gmail.com>
2023-12-14 01:15:38 +0100

    day13 better solution

--- day13/solution.py
+++ day13/solution.py
@@ -1,44 +1,27 @@
-MEMORY = dict()
-SUBS = {"#": ".", ".": "#"}
-
-
-def symmetry(p, skip=None):
+def symmetry(p):
     N = len(p)
     for i in range(1, N):
-        if skip is not None and i == skip:
-            continue
         if all(p[i - j - 1] == p[i + j] for j in range(min(i, N - i))):
             return i
     return 0
 
 
-def summarize(pattern, skip=None):
+def smudge_symmetry(p):
+    N = len(p)
+    for i in range(1, N):
+        if sum(hamming(p[i - j - 1], p[i + j]) for j in range(min(i, N - i))) == 1:
+            return i
+    return 0
+
+
+def hamming(X, Y):
+    return sum(x != y for x, y in zip(X, Y))
+
+
+def summarize(pattern, func):
     rows = pattern.split()
     cols = ["".join(line) for line in zip(*rows)]
-    if skip:
-        idx, direction = skip
-        if direction == "h":
-            h_symmetry = symmetry(rows, idx)
-            v_symmetry = symmetry(cols)
-        else:
-            h_symmetry = symmetry(rows)
-            v_symmetry = symmetry(cols, idx)
-    else:
-        if h_symmetry := symmetry(rows):
-            MEMORY[pattern] = h_symmetry, "h"
-        if v_symmetry := symmetry(cols):
-            MEMORY[pattern] = v_symmetry, "v"
-    return 100 * h_symmetry + v_symmetry
-
-
-def smudge(pattern):
-    skip = MEMORY[pattern]
-    for i, c in enumerate(pattern):
-        if c not in SUBS:  # skip newline char
-            continue
-        smudged = pattern[:i] + SUBS[c] + pattern[i + 1:]
-        if s := summarize(smudged, skip):
-            return s
+    return 100 * func(rows) + func(cols)
 
 
 with open("data") as f:
@@ -46,8 +29,8 @@ with open("data") as f:
 
 
 # ========= PART 1 =========
-print(sum(summarize(pattern) for pattern in patterns))
+print(sum(summarize(pattern, symmetry) for pattern in patterns))
 
 
 # ========= PART 2 =========
-print(sum(smudge(pattern) for pattern in patterns))
+print(sum(summarize(pattern, smudge_symmetry) for pattern in patterns))
