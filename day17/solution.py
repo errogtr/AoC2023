7e9ae32246bb8da749b06c2e2aaa56681a2a86e4
enrico <enrico.dsf@gmail.com>
2023-12-21 19:45:20 +0100

    FINALLY!!!! day17 p2 solution

--- day17/solution.py
+++ day17/solution.py
@@ -21,19 +21,23 @@ def in_grid(p, Lx, Ly):
     return True if 0 <= x <= Lx and 0 <= y <= Ly else False
 
 
-def next_moves(current, go_to, fuel, Lx, Ly):
+def next_moves(current, came_from, steps, max_steps, min_steps):
+    if 0 < steps < min_steps:
+        return [(add(current, came_from), came_from, steps + 1)]
+
     moves = list()
-    for direction in MOVES[go_to]:
-        move_fuel = fuel
-        if direction == go_to:
-            if fuel == 0:
+    for direction in MOVES[came_from]:
+        next_steps = steps
+        if direction == came_from:
+            if steps == max_steps:
                 continue
-            move_fuel -= 1
+            next_steps += 1
         else:
-            move_fuel = 2
+            next_steps = 1
         node = add(current, direction)
         if in_grid(node, Lx, Ly):
-            moves.append((node, direction, move_fuel))
+            moves.append((node, direction, next_steps))
+
     return moves
 
 
@@ -41,26 +45,29 @@ def get_cost(x, y, heatmap):
     return int(heatmap[y][x])
 
 
-def least_heat_loss(heatmap, Lx, Ly):
-    start, fuel, cost = (0, 0), 3, 0
-    init_state = start, start, fuel
+def least_heat_loss(heatmap, max_steps, min_steps, Lx, Ly):
+    start, steps, cost = (0, 0), 0, 0
+    init_state = start, start, steps
     nodes_cost = {init_state: cost}
     frontier = []
     heappush(frontier, (cost, init_state))
     while frontier:
         cost, state = heappop(frontier)
-        current, go_to, fuel = state
+        current, came_from, steps = state
 
-        if current == (Lx, Ly):
-            return cost
+        if current == (Lx, Ly) and steps >= min_steps:
+            break
 
-        for node, move, node_fuel in next_moves(current, go_to, fuel, Lx, Ly):
+        for node, move, node_steps in next_moves(current, came_from, steps, max_steps, min_steps):
+            if not in_grid(node, Lx, Ly):
+                continue
             node_cost = get_cost(*node, heatmap) + cost
-            node_state = (node, move, node_fuel)
+            node_state = (node, move, node_steps)
             if (node_state not in nodes_cost) or node_cost < nodes_cost[node_state]:
                 nodes_cost[node_state] = node_cost
                 state = (node_cost, node_state)
                 heappush(frontier, state)
+    return cost
 
 
 with open("data") as f:
@@ -70,4 +77,7 @@ Ly = len(heatmap) - 1
 
 
 # ==== PART 1 ====
-print(least_heat_loss(heatmap, Lx, Ly))
+print(least_heat_loss(heatmap, 3, 1, Lx, Ly))
+
+# ==== PART 2 ====
+print(least_heat_loss(heatmap, 10, 4, Lx, Ly))
